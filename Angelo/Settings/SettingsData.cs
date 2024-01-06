3dc8376912038e63142bb839b55de22080c7087c
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-01-06 18:48:47 +0100

    More sensible defaults

--- Angelo/Settings/SettingsData.cs
+++ Angelo/Settings/SettingsData.cs
@@ -5,13 +5,13 @@ namespace Angelo.Settings
 {
     internal class SettingsData
     {
-        public Setting<int> Sensitivity { get; } = new(150);
+        public Setting<int> Sensitivity { get; } = new(100);
         public Setting<int> Threshold { get; } = new(200);
         public Setting<bool> UseLure { get; } = new(false);
         public Setting<KeyBind> FishingKey { get; } = new(new KeyBind((byte)KeyInterop.VirtualKeyFromKey(Key.D6), KBM.KeyboardModifiers.CTRL));
         public Setting<KeyBind> LureKey { get; } = new(new KeyBind((byte)KeyInterop.VirtualKeyFromKey(Key.D7), KBM.KeyboardModifiers.CTRL));
-        public Setting<int> BobberHue { get; } = new(0);
-        public Setting<int> BobberHueTolerance { get; } = new(15);
+        public Setting<int> BobberHue { get; } = new(6);
+        public Setting<int> BobberHueTolerance { get; } = new(10);
         public Setting<int> BobberPixels { get; } = new(150);
     }
 }
